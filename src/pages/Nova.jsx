51736822a638f26f90f21a69cdbfb55ae31fe33f
LUCCA BARCELOS CRAVO <202402630776@alunos.ibmec.edu.br>
2024-10-03 17:08:23 -0300

    a

---
+++ src/pages/Nova.jsx
@@ -0,0 +1,10 @@
+import NovoComponente from "../components/NovoComponente/NovoComponente";
+const Nova = () => {
+    return (<>
+    <h1>Titulo da pagina</h1>
+    <p>Oi</p>
+    <NovoComponente/>
+    </>);
+}
+export default Nova;
+
